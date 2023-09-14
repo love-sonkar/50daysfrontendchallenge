395277a675789b880af565c86d0c88733136d5ba
love-sonkar <loveboss806@gmail.com>
2023-09-14 18:07:20 +0530

    some bug fixes

--- day14modal/js.js
+++ day14modal/js.js
@@ -2,16 +2,18 @@ const modal = document.querySelector(".modal");
 const modal_body = document.querySelector(".modal-body");
 const button = document.querySelector(".btn-delete");
 const delete_modal = document.querySelector(".model-delete");
+const cancel_modal = document.querySelector(".cancel-modal");
 
-button.addEventListener("click", () => {
-  console.log("clik");
+button.addEventListener("click", ModalOpen);
+delete_modal.addEventListener("click", ModalOff);
+cancel_modal.addEventListener("click", ModalOff);
+
+function ModalOpen() {
   modal.classList.add("modal-active");
   modal_body.classList.add("modal-body-active");
-  modal_body.classList.remove("modal-body-active-hide");
-});
+}
 
-delete_modal.addEventListener("click", () => {
+function ModalOff() {
   modal.classList.remove("modal-active");
   modal_body.classList.remove("modal-body-active");
-  modal_body.classList.add("modal-body-active-hide");
-});
+}
