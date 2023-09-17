fed2087839f1df8c5fd043fdc3807e388f06500e
love-sonkar <loveboss806@gmail.com>
2023-09-17 20:32:40 +0530

    working searchbox added

--- day17workingsearchbar/js.js
+++ day17workingsearchbar/js.js
@@ -1,12 +1,19 @@
-let data = [];
+let user = [];
+
+const imageData = [
+  "https://images.unsplash.com/photo-1474447976065-67d23accb1e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  "https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  "https://images.unsplash.com/photo-1455274111113-575d080ce8cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+];
 
 const fetchFunction = async () => {
   try {
     let dataget = await fetch("https://jsonplaceholder.typicode.com/users");
     let json = await dataget.json();
-    data = json;
-    displaynewdata();
-    console.log(data);
+    displaynewdata(json);
+    user = json;
   } catch (error) {
     console.log(error);
   }
@@ -14,14 +21,17 @@ const fetchFunction = async () => {
 
 fetchFunction();
 
-const displaynewdata = () => {
+const displaynewdata = (data) => {
   const cardBody = document.querySelector(".card-body");
-
-  data.forEach((item) => {
+  if (data.length == 0) {
+    return (cardBody.innerHTML = `<h2 class="msg">No User Found</h2>`);
+  }
+  const mapdata = data.map((item) => {
+    const randomNumber = Math.floor(Math.random() * imageData.length);
     const i = `<div class="card-item">
 <div class="profile">
   <img
-    src="https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&w=400"
+    src="${imageData[randomNumber]}"
     alt=""
   />
   </div>
@@ -31,7 +41,20 @@ const displaynewdata = () => {
   <p class="address">${item.company.catchPhrase}</p>
   </div>
   </div>`;
-
-    cardBody.innerHTML += i;
+    return i;
   });
+  cardBody.innerHTML = mapdata;
 };
+
+const input = document.querySelector(".input");
+
+input.addEventListener("keyup", SearchUser);
+
+function SearchUser() {
+  let lowercase = input.value.toLowerCase();
+  const searchfilter = user.filter((item) => {
+    const itemname = item.name.toLowerCase();
+    if (itemname.includes(lowercase)) return item;
+  });
+  displaynewdata(searchfilter);
+}
