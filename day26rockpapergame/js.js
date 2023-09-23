2696940593faf6e1b3ea5788e362f6f3aee674fb
love-sonkar <loveboss806@gmail.com>
2023-09-23 16:15:59 +0530

    added rock paper game

---
+++ day26rockpapergame/js.js
@@ -0,0 +1,54 @@
+const result = document.querySelector(".result");
+const yourchoice = document.querySelector(".choice");
+const computer = document.querySelector(".computer");
+const Cresult = document.querySelector(".computerresult");
+const Uresult = document.querySelector(".userresult");
+
+const button = document.querySelectorAll(".btn");
+let userCounter = 0;
+let coumputerCounter = 0;
+function PrintResult(comp, user) {
+  Cresult.innerText = comp;
+  Uresult.innerText = user;
+}
+
+PrintResult(0, 0);
+
+const Options = ["rock", "paper", "scissors"];
+
+button.forEach((item) => {
+  item.addEventListener("click", () => {
+    const checkoption = Math.floor(Math.random() * Options.length);
+    const value = item.getAttribute("data-value");
+    computer.innerText = Options[checkoption];
+    if (Options[checkoption] === value) {
+      result.innerText = "Match Is Draw..!";
+    } else if (Options[checkoption] === "rock") {
+      if (value === "scissors") {
+        result.innerText = "You Lost..!";
+        PrintResult(coumputerCounter++, userCounter);
+      } else {
+        result.innerText = "You won..!";
+        userCounter++
+        PrintResult(coumputerCounter, userCounter);
+      }
+    } else if (Options[checkoption] === "paper") {
+      if (value === "scissors") {
+        result.innerText = "You Won..!";
+        PrintResult(coumputerCounter, userCounter++);
+      } else {
+        result.innerText = "You Lost..!";
+        PrintResult(coumputerCounter++, userCounter);
+      }
+    } else if (Options[checkoption] === "scissors") {
+      if (value === "paper") {
+        result.innerText = "You Lost..!";
+        PrintResult(coumputerCounter++, userCounter);
+      } else {
+        result.innerText = "You Won..!";
+        PrintResult(coumputerCounter, userCounter++);
+      }
+    }
+    yourchoice.innerText = value;
+  });
+});
