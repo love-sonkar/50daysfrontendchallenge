785ea0489a60a64177674e667e64c913a3612cf5
love-sonkar <loveboss806@gmail.com>
2023-09-24 22:07:14 +0530

    added animation in day27toastnotification

--- day27toastnotification/js.js
+++ day27toastnotification/js.js
@@ -1,17 +1,20 @@
 const btn = document.querySelector(".btn");
 const toast = document.querySelector(".toast");
 
-const color = ["success","error","alert"]
+const color = ["success", "error", "alert"];
 
-const Generaterandom = ()=>{
-  return Math.floor(Math.random()*color.length);
-}
+const Generaterandom = () => {
+  return Math.floor(Math.random() * color.length);
+};
 
 btn.addEventListener("click", () => {
+  const generateClass = color[Generaterandom()];
+
   const notification = document.createElement("div");
   notification.classList.add("cover");
-  notification.classList.add(color[Generaterandom()])
-  notification.innerText = "This is Notification";
+  notification.classList.add(generateClass);
+
+  notification.innerText = `${generateClass} Notification`;
 
   toast.append(notification);
 
