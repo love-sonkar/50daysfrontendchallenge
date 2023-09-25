17e1edbf25d12e7465ff5bf02c0ce58b951020f1
love-sonkar <loveboss806@gmail.com>
2023-09-25 21:03:31 +0530

    fully functional random jokes project added

--- day28randomjokes/js.js
+++ day28randomjokes/js.js
@@ -1,20 +1,24 @@
 const btn = document.querySelector(".btn");
 const joketag = document.querySelector(".joke");
 
-btn.addEventListener("click", () => {
-  GenerateJokes();
-});
+btn.addEventListener("click", GenerateJokes);
 
 async function GenerateJokes() {
-  btn.setAttribute("disabled", true);
+  btn.disabled = true;
+  btn.innerText = "Loading...";
   try {
-    const res = await fetch("https://icanhazdadjoke.com");
+    const res = await fetch("https://icanhazdadjoke.com", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
     const data = await res.json();
     joketag.innerText = data.joke;
   } catch (error) {
     console.log(error);
   }
-  btn.setAttribute("disabled", false);
+  btn.disabled = false;
+  btn.innerText = "Another Jokes";
 }
 
 GenerateJokes();
