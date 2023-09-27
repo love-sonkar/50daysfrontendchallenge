995d4a8aecc9eaa894f1e1b3f4ec2cf97db0dc83
love-sonkar <loveboss806@gmail.com>
2023-09-27 22:46:24 +0530

    some changes

--- day32mobilenavigation/js.js
+++ day32mobilenavigation/js.js
@@ -1,23 +1,12 @@
 const hamopen = document.querySelector(".hammenu");
 const hamclose = document.querySelector(".close");
+const navitems = document.querySelectorAll(".select");
 
-const navigation = document.querySelector(".navigation");
-const nav2 = document.querySelector(".nav2");
-const nav = document.querySelector(".nav");
-
-
-
-hamopen.addEventListener("click",()=>{
-    navigation.classList.add("show")
-    nav2.classList.add("show")
-    nav.classList.add("show")
-
-})
-
-
-hamclose.addEventListener("click",()=>{
-    navigation.classList.remove("show")
-    nav2.classList.remove("show")
-    nav.classList.remove("show")
-
-})
+navitems.forEach((item) => {
+  hamopen.addEventListener("click", () => {
+    item.classList.add("show");
+  });
+  hamclose.addEventListener("click", () => {
+    item.classList.remove("show");
+  });
+});
