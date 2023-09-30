1faa904d97f235913db71e51d89a47b63674e20a
love-sonkar <loveboss806@gmail.com>
2023-09-30 20:32:26 +0530

    new project added

---
+++ day34incrementanimation/js.js
@@ -0,0 +1,26 @@
+const text = document.querySelector(".text");
+const btn = document.querySelector(".btn");
+
+let endnumber = 500;
+
+btn.addEventListener("click", ()=>{
+text.innerHTML = 00;
+    shownumber(1)
+
+}
+    );
+
+function shownumber(num) {
+  if (num == endnumber) {
+    return;
+  } else {
+    setTimeout(() => {
+      let increment = ++num;
+      text.innerHTML = increment;
+      shownumber(increment);
+    }, 0.1);
+  }
+}
+
+shownumber(1);
+
