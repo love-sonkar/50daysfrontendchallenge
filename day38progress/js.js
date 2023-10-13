d70f7af7ebb58c45b0bc00cee6e407f188ebe2d7
love-sonkar <loveboss806@gmail.com>
2023-10-13 22:26:20 +0530

    fully functional step animation

--- day38progress/js.js
+++ day38progress/js.js
@@ -1,46 +1,52 @@
 const nextbutton = document.querySelector(".nextbutton");
 const prevbutton = document.querySelector(".prevbutton");
 const line = document.querySelector(".line");
+const item2 = document.querySelector(".item2")
+const item3 = document.querySelector(".item3")
+const item4 = document.querySelector(".item4")
+
 
 let count = 1;
 
 nextbutton.addEventListener("click", () => {
-    CountIncrease()
+  CountIncrease();
   if (count == 2) {
     line.style.right = "62%";
+    item2.classList.add("active")
   } else if (count == 3) {
     line.style.right = "35%";
+    item3.classList.add("active")
   } else if (count == 4) {
     line.style.right = "0%";
-  } 
-
-
-
+    item4.classList.add("active")
+  }
 });
 
 prevbutton.addEventListener("click", () => {
-    CountDecrease()
+  CountDecrease();
   if (count == 3) {
     line.style.right = "35%";
+    item4.classList.remove("active")
   } else if (count == 2) {
     line.style.right = "62%";
+    item3.classList.remove("active")
   } else if (count == 1) {
     line.style.right = "100%";
-  } 
-
-  
-
+    item2.classList.remove("active")
+  }
 });
 
-function CountIncrease(){
-    count++
-    if(count >= 4){
-        count = 4
-    }
+function CountIncrease() {
+  count++;
+  if (count >= 4) {
+    count = 4;
+  }
+  console.log(count)
 }
-function CountDecrease(){
-    count--
-    if(count <=1){
-        count = 1
-    }
+function CountDecrease() {
+  count--;
+  if (count <= 1) {
+    count = 1;
+  }
+  console.log(count)
 }
