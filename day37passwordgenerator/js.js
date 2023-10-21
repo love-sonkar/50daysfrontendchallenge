1e348cdf727434d8bdc03119883943e3edeb3b7b
love-sonkar <loveboss806@gmail.com>
2023-10-21 15:39:46 +0530

    ui and some functionlity done

--- day37passwordgenerator/js.js
+++ day37passwordgenerator/js.js
@@ -1,33 +1,48 @@
 const btn = document.querySelector(".btn");
+const reset = document.querySelector(".reset");
+
 const showpass = document.querySelector(".showpass");
-const number = document.querySelector(".number");
+const checkToggles = document.querySelectorAll(".checkToggle");
 const range = document.querySelector(".range");
 const labelrange = document.querySelector(".labelrange");
+const number = document.querySelector("#number");
+const capital = document.querySelector("#capitalLetter")
+const special = document.querySelector("#specialLetter")
 
 
 let passlength = 5;
 
-range.addEventListener("click",(e)=>{
-    passlength = e.target.value
-    PasswordGenerator()
-    lebelrangeText()
-})
+range.addEventListener("click", (e) => {
+  passlength = e.target.value;
+  PasswordGenerator();
+  lebelrangeText();
+});
 
-function lebelrangeText (){
-    labelrange.innerHTML = passlength;
+function lebelrangeText() {
+  labelrange.innerHTML = passlength;
 }
 
-lebelrangeText()
+lebelrangeText();
 
 btn.addEventListener("click", PasswordGenerator);
+reset.addEventListener("click", PasswordGenerator);
+
 
 function PasswordGenerator() {
   let orignalPss = "";
-  let alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let alphabets = "abcdefghijklmnopqrstuvwxyz";
+  let CapitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let SpecialChar = "$#@!%^*()_~``{}><"
   let numbers = "0123456789";
   let genpass = "";
-  if (NumberTrueFalseCheck() == "true") {
+  if (number.getAttribute("data-number") == "true") {
     genpass += numbers;
+  }
+  if (capital.getAttribute("data-number") == "true") {
+    genpass += CapitalLetter;
+  }
+  if (special.getAttribute("data-number") == "true") {
+    genpass += SpecialChar;
   }
 
   genpass += alphabets;
@@ -35,22 +50,19 @@ function PasswordGenerator() {
     let randomenumber = Math.floor(Math.random() * genpass.length);
     orignalPss += genpass.charAt(randomenumber);
   }
-  showpass.setAttribute("value",orignalPss)
+  showpass.setAttribute("value", orignalPss);
   genpass = "";
 }
-
-number.addEventListener("click", () => {
-  const getattr = NumberTrueFalseCheck();
-  if (getattr == "true") {
-    number.setAttribute("data-number", false);
-  } else {
-    number.setAttribute("data-number", true);
-  }
+checkToggles.forEach((Toggle) => {
+  Toggle.addEventListener("click", () => {
+    Toggle.children[0].classList.toggle("trueToggle");
+    const getattr = Toggle.getAttribute("data-number");
+    if (getattr == "true") {
+      Toggle.setAttribute("data-number", false);
+    } else {
+      Toggle.setAttribute("data-number", true);
+    }
+  });
 });
 
-function NumberTrueFalseCheck() {
-  return number.getAttribute("data-number");
-}
-
-
-PasswordGenerator()
+PasswordGenerator();
