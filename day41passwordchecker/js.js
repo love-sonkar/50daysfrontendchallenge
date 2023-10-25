3549d74d0941420d5ca8813f8c6ac485afda38e1
love-sonkar <loveboss806@gmail.com>
2023-10-25 22:47:27 +0530

    added day41 password checker

--- day41passwordchecker/js.js
+++ day41passwordchecker/js.js
@@ -8,15 +8,22 @@ input.addEventListener("input", (e) => {
   const value = e.target.value;
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let number = "1234567890";
+  let specialchr = "!@#$%^&*()_+";
   pass.innerHTML = "Weak";
+  Imge.style.filter = "blur(100px)"
 
-  for (let i = 0; i < value.length; i++) {
-    if (alphabet.includes(value[i])) {
-      pass.innerHTML = "Medium";
-    } else if (number.includes(value[i])) {
-      if (value.includes(alphabet)) {
-        pass.innerHTML = "Strong";
+  for (let i = 0; i <= value.length; i++) {
+    console.log(value[i])
+    if (value.includes(alphabet[i])) {
+      if(value.includes(number[i])){
+        pass.innerHTML = "Medium";
+        Imge.style.filter = "blur(50px)"
       }
+    } 
+    else if (value.includes(specialchr[i])) {
+      pass.innerHTML = "Strong";
+      Imge.style.filter = "blur(0)"
+
     }
   }
 });
