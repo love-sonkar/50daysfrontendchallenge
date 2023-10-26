f3fc612752454599d117d25bc7d336e725fe0ff9
love-sonkar <loveboss806@gmail.com>
2023-10-26 22:18:53 +0530

    added a card chaning animation with loading screen

--- day42changingcard/js.js
+++ day42changingcard/js.js
@@ -1,43 +1,43 @@
 const data = [
   {
     textContent: "Genius is one percent inspiration and ninety-nine percent perspiration.",
-    authorContent: "Author : Thomas Edison"
+    authorContent: "Thomas Edison"
   },
   {
     textContent: "You can observe a lot just by watching.",
-    authorContent: "Author : Yogi Berra"
+    authorContent: "Yogi Berra"
   },
   {
     textContent: "A house divided against itself cannot stand.",
-    authorContent: "Author : Abraham Lincoln"
+    authorContent: "Abraham Lincoln"
   },
   {
     textContent: "Difficulties increase the nearer we get to the goal.",
-    authorContent: "Author : Johann Wolfgang von Goethe"
+    authorContent: "Johann Wolfgang von Goethe"
   },
   {
     textContent: "Fate is in your hands and no one elses",
-    authorContent: "Author : Byron Pulsifer"
+    authorContent: "Byron Pulsifer"
   },
   {
     textContent: "Be the chief but never the lord.",
-    authorContent: "Author : Lao Tzu"
+    authorContent: "Lao Tzu"
   },
   {
     textContent: "Nothing happens unless first we dream.",
-    authorContent: "Author : Carl Sandburg"
+    authorContent: "Carl Sandburg"
   },
   {
     textContent: "Well begun is half done.",
-    authorContent: "Author : Aristotle"
+    authorContent: "Aristotle"
   },
   {
     textContent: "Life is a learning experience, only if you learn.",
-    authorContent: "Author : Yogi Berra"
+    authorContent: "Yogi Berra"
   },
   {
     textContent: "Self-complacency is fatal to progress.",
-    authorContent: "Author : Margaret Sangster"
+    authorContent: "Margaret Sangster"
   },
 ];
 
