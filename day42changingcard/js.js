7324c28a89d2e6ad6fa7fe13b94db8eb84d76029
love-sonkar <loveboss806@gmail.com>
2023-10-26 20:16:35 +0530

    some update

--- day42changingcard/js.js
+++ day42changingcard/js.js
@@ -1,23 +1,49 @@
 const data = [
   {
-    textContent:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex repellat necessitatibus rem voluptatibus temporibus cumque veniam",
+    textContent: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    authorContent: "Author : Thomas Edison"
   },
   {
-    textContent:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam magni molestiae minima amet. Quidem eveniet dolores quia labore laboriosam error!",
+    textContent: "You can observe a lot just by watching.",
+    authorContent: "Author : Yogi Berra"
   },
   {
-    textContent:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptate maxime facere?",
+    textContent: "A house divided against itself cannot stand.",
+    authorContent: "Author : Abraham Lincoln"
   },
   {
-    textContent:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis illum non, dolor esse vel in!",
+    textContent: "Difficulties increase the nearer we get to the goal.",
+    authorContent: "Author : Johann Wolfgang von Goethe"
+  },
+  {
+    textContent: "Fate is in your hands and no one elses",
+    authorContent: "Author : Byron Pulsifer"
+  },
+  {
+    textContent: "Be the chief but never the lord.",
+    authorContent: "Author : Lao Tzu"
+  },
+  {
+    textContent: "Nothing happens unless first we dream.",
+    authorContent: "Author : Carl Sandburg"
+  },
+  {
+    textContent: "Well begun is half done.",
+    authorContent: "Author : Aristotle"
+  },
+  {
+    textContent: "Life is a learning experience, only if you learn.",
+    authorContent: "Author : Yogi Berra"
+  },
+  {
+    textContent: "Self-complacency is fatal to progress.",
+    authorContent: "Author : Margaret Sangster"
   },
 ];
 
 const text = document.querySelector(".textcontent");
+const author = document.querySelector(".author");
+
 
 let i = 0;
 
@@ -27,6 +53,7 @@ function GenerateRandomeContent() {
     i = 0;
   }
   text.innerHTML = data[i].textContent;
+  author.innerHTML = data[i].authorContent;
 }
 
 setInterval(() => {
