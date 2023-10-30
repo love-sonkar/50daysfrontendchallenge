3522e863f25ce49e15412656b200996f916c54e9
love-sonkar <loveboss806@gmail.com>
2023-10-30 18:41:33 +0530

    added footer with autometic date

--- day39travellandingpage/js.js
+++ day39travellandingpage/js.js
@@ -1,9 +1,15 @@
 const ham = document.querySelector(".ham")
 const header = document.querySelector(".header")
 const mobilenav =document.querySelector(".mobilenav");
+const date =document.querySelector(".date");
+
 
 ham.addEventListener("click",()=>{
     header.classList.toggle("headerborder")
     header.classList.toggle("ani")
     mobilenav.classList.toggle("navheight")
 })
+
+
+const newDate = new Date().getFullYear();
+date.innerHTML = newDate
