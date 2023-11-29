121cbeed8ecc9e68c9f345727f5af8e7245bd1c8
love-sonkar <loveboss806@gmail.com>
2023-11-29 10:44:27 +0530

    adding validation in form

---
+++ day25loginpage/js.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    
+})
