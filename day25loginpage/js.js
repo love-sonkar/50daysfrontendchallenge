fb16611721f676cb0e68985ee9e4d944314c9fce
love-sonkar <loveboss806@gmail.com>
2023-11-29 11:01:37 +0530

    some validation added

--- day25loginpage/js.js
+++ day25loginpage/js.js
@@ -3,7 +3,32 @@ const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
+const errorObj = {
+     usernameError : document.querySelector(".usernameError"),
+     emailError : document.querySelector(".emailError"),
+     passwordError: document.querySelector(".passwordError"),
+}
+
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
-    
+    const {usernameError,emailError,passwordError} = errorObj;
+   const usernameValue = username.value;
+   const emailValue =email.value;
+   const passwordValue = password.value;
+
+   if( usernameValue.length < 4){
+     usernameError.innerHTML = "Username Must be 4 character";
+   }else{
+     usernameError.innerHTML = ""
+   }
+   if(!emailValue){
+    emailError.innerHTML = "Please Enter Your Email";
+   }else{
+    const emailRegex = 'com' 
+    if(emailRegex.includes(emailValue)){
+        emailError.innerHTML=""
+    }
+    emailError.innerHTML = "not valid email"
+   }
+
 })
