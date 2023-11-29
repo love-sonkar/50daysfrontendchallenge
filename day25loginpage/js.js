12ef21d68b4dd6faf43b72dcd6444409e2594981
love-sonkar <loveboss806@gmail.com>
2023-11-29 11:09:21 +0530

    validation done

--- day25loginpage/js.js
+++ day25loginpage/js.js
@@ -4,31 +4,51 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
 const errorObj = {
-     usernameError : document.querySelector(".usernameError"),
-     emailError : document.querySelector(".emailError"),
-     passwordError: document.querySelector(".passwordError"),
-}
+  usernameError: document.querySelector(".usernameError"),
+  emailError: document.querySelector(".emailError"),
+  passwordError: document.querySelector(".passwordError"),
+};
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    const {usernameError,emailError,passwordError} = errorObj;
-   const usernameValue = username.value;
-   const emailValue =email.value;
-   const passwordValue = password.value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { usernameError, emailError, passwordError } = errorObj;
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
 
-   if( usernameValue.length < 4){
-     usernameError.innerHTML = "Username Must be 4 character";
-   }else{
-     usernameError.innerHTML = ""
-   }
-   if(!emailValue){
-    emailError.innerHTML = "Please Enter Your Email";
-   }else{
-    const emailRegex = 'com' 
-    if(emailRegex.includes(emailValue)){
-        emailError.innerHTML=""
+  //username validation
+  if (!usernameValue) {
+    usernameError.innerHTML = "Please Enter Username";
+  } else {
+    if (usernameValue.length < 4) {
+      usernameError.innerHTML = "Username Must be 4 character";
+    } else {
+      usernameError.innerHTML = "";
     }
-    emailError.innerHTML = "not valid email"
-   }
+  }
 
-})
+  // email validation
+  if (!emailValue) {
+    emailError.innerHTML = "Please Enter Your Email";
+  } else {
+    const emailRegex = ".com";
+    if (emailValue.match(emailRegex)) {
+      emailError.innerHTML = "";
+    } else {
+      emailError.innerHTML = "not valid email";
+    }
+  }
+
+  // password validation
+  if (!passwordValue) {
+    passwordError.innerHTML = "Enter Your password";
+  }else{
+    if(passwordValue.length < 4){
+    passwordError.innerHTML = "Password lenght must be 4";
+    }else{
+    passwordError.innerHTML = "";
+    }
+  }
+
+
+});
